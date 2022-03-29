9bbbdf5921447405ee0b73ac989283cdae42f390
Supreeth-Athreyas <82948637+Supreeth-Athreyas@users.noreply.github.com>
2022-03-29 22:37:45 +0530

    Delete URL_update.ps1

--- URL_update.ps1
+++
@@ -1,4 +0,0 @@
-param ($Filepath, $OldURL, $NewURL)
-$ConfigFilePath = $Filepath
-$content = [System.IO.File]::ReadAllText($ConfigFilepath).Replace("$OldURL","$NewURL")
-[System.IO.File]::WriteAllText($ConfigFilePath, $content)
